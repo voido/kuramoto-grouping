e35689806ca9ccfabd2e43555bfcc6ea65ee5598
Martin Meier <mmeier@techfak.uni-bielefeld.de>
2015-10-27 15:39:09 +0100

    added some members

--- kuramoto_grouping.cc
+++ kuramoto_grouping.cc
@@ -6,17 +6,29 @@ typedef std::vector<float> state_type;
 
 
 struct osc_ensemble {
+	osc_ensemble(size_t num_osc, size_t num_layer, double K) :
+	num(num_osc), layer(num_layer), K(K) {
+	
+	}
 
+	~osc_ensemble() {}
 
 	void operator()(state_type &x, state_type &dxdt, double t) {
 
 	}
 
+	size_t num;
+	size_t layer;
+	double K;
 };
 
 int main(int argc, char* argv[]) {
+	
+	size_t units = 100;
+	size_t layer = 10;
+	double K = 2.0;
 
-	osc_ensemble net;
+	osc_ensemble net(units, layer, K);
 	state_type x;
 
 
