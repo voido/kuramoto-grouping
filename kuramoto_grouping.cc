af32e475aa6709ee9cea060c7951158226b26346
Martin Meier <mmeier@techfak.uni-bielefeld.de>
2015-10-27 15:58:35 +0100

    frequency assignment

--- kuramoto_grouping.cc
+++ kuramoto_grouping.cc
@@ -43,7 +43,7 @@ typedef std::vector<osc> state_type;
 struct osc_ensemble {
 	osc_ensemble(size_t num_osc, size_t num_layer, double K) :
 	num(num_osc), layer(num_layer), K(K) {
-	
+		support.resize(layer);
 	}
 
 	~osc_ensemble() {}
@@ -55,11 +55,14 @@ struct osc_ensemble {
 			const osc &mx = x[i];
 			osc &mdxdt = dxdt[i];
 			double tmp = 0.0;
+			std::fill(support.begin(), support.end(), 0.0);
 
 			for(size_t j=0; j<n; j++) {
 				tmp += f(i,j)*sin(x[j].phase-mx.phase);
 			}
-			mdxdt.phase = K*tmp; //TODO: omega_alpha
+			mdxdt.phase = K*tmp; 
+			int nf = std::distance(support.begin(), std::max_element(support.begin(), support.end()));
+			x[i].frequency = nf;
 		}
 	}
 
@@ -70,6 +73,7 @@ struct osc_ensemble {
 	size_t num;
 	size_t layer;
 	double K;
+	std::vector<double> support;
 };
 
 int main(int argc, char* argv[]) {
